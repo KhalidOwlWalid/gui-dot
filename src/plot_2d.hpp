f6ff26f650f88f2adf8497e3f6d4c7224ba5d139
KhalidOwlWalid <khalidowlwalid@gmail.com>
2024-12-24 23:22:02 +0000

    Archive old .gdextension, and migrate .so build to 4.3

---
+++ src/plot_2d.hpp
@@ -0,0 +1,35 @@
+#ifndef PLOT_2D_HPP
+#define PLOT_2D_HPP
+
+#include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/variant/vector2.hpp>
+
+namespace godot {
+
+class Plot_2D : public Control {
+
+  GDCLASS(Plot_2D, Control);
+
+  public:
+
+    Plot_2D();
+    ~Plot_2D();
+    // void _process(double delta) override;
+
+    void set_background_color(Color color);
+    Color get_background_color();
+
+    void _draw() override;
+
+  protected:
+    static void _bind_methods();
+
+  private:
+    Vector2 _graph_size;
+    Color _background_color;
+
+};
+
+}
+
+#endif
