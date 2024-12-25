6781629e1709d1dc36f335cc396957f2994e07f0
KhalidOwlWalid <khalidowlwalid@gmail.com>
2024-12-25 10:15:32 +0000

    Testing some gdextension features

--- src/gdexample.cpp
+++ src/gdexample.cpp
@@ -10,8 +10,12 @@ void GDExample::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_speed"), &GDExample::get_speed);
 	ClassDB::bind_method(D_METHOD("set_speed", "p_speed"), &GDExample::set_speed);
 
+	ClassDB::bind_method(D_METHOD("get_test"), &GDExample::get_test);
+	ClassDB::bind_method(D_METHOD("set_test", "test"), &GDExample::set_test);
+
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "amplitude"), "set_amplitude", "get_amplitude");
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "speed", PROPERTY_HINT_RANGE, "0,20,0.01"), "set_speed", "get_speed");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "_test", PROPERTY_HINT_RANGE, "0,20,0.01"), "set_test", "get_test");
 
 	ADD_SIGNAL(MethodInfo("position_changed", PropertyInfo(Variant::OBJECT, "node"), PropertyInfo(Variant::VECTOR2, "new_pos")));
 
@@ -23,6 +27,7 @@ GDExample::GDExample() {
 	amplitude = 10.0;
 	speed = 1.0;
 	time_emit = 0.0;
+	_test = 0.0;
 }
 
 GDExample::~GDExample() {
@@ -60,4 +65,12 @@ void GDExample::set_speed(const double p_speed) {
 
 double GDExample::get_speed() const {
 	return speed;
+}
+
+double GDExample::get_test() const {
+	return _test;
+}
+
+void GDExample::set_test(const double test) {
+	_test = test;
 }
--- src/plot_2d.cpp
+++ src/plot_2d.cpp
@@ -8,26 +8,41 @@ void Plot_2D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_background_color"), &Plot_2D::get_background_color);
 	ClassDB::bind_method(D_METHOD("set_background_color", "color"), &Plot_2D::set_background_color);
 
-	ADD_PROPERTY(PropertyInfo(Variant::COLOR, "_background_color"), "get_background_color", "set_background_color");
+
+	ClassDB::bind_method(D_METHOD("get_amplitude"), &Plot_2D::get_amplitude);
+	ClassDB::bind_method(D_METHOD("set_amplitude", "p_amplitude"), &Plot_2D::set_amplitude);
+
+  // Note: Please do not flip between setter methods and getter methods in the ADD_PROPERTY definition
+	ADD_PROPERTY(PropertyInfo(Variant::COLOR, "_background_color"), "set_background_color", "get_background_color");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "_amplitude"), "set_amplitude", "get_amplitude");
+	// ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "_amplitude", PROPERTY_HINT_RANGE, "0,20,0.01"), "set_amplitude", "get_amplitude");
 }
 
 Plot_2D::Plot_2D() {
   _background_color = Color(0.2, 0.2, 0.2, 1.0);
+  _amplitude = 0.0;
 }
 
 Plot_2D::~Plot_2D() {
 }
 
-Color Plot_2D::get_background_color() {
+Color Plot_2D::get_background_color() const {
   return _background_color;
 }
 
-void Plot_2D::set_background_color(Color color) {
+void Plot_2D::set_background_color(const Color color) {
   _background_color = color;
 }
 
-void Plot_2D::_draw() {
+double Plot_2D::get_amplitude() const {
+  return _amplitude;
+}
 
+void Plot_2D::set_amplitude(const double p_amplitude) {
+  _amplitude = p_amplitude;
+}
+
+void Plot_2D::_draw() {
   // Draw the background
   Rect2 background_rect = Rect2(Vector2(0, 0), get_size());
   draw_rect(background_rect, _background_color);
