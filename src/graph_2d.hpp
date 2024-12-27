057b9025b58942c40d0191731ebde8f4f6744b10
KhalidOwlWalid <khalidowlwalid@gmail.com>
2024-12-27 12:12:04 +0000

    [feature] Add font to plot

--- src/graph_2d.hpp
+++ src/graph_2d.hpp
@@ -4,10 +4,56 @@
 #include <godot_cpp/classes/control.hpp>
 #include <godot_cpp/variant/vector2.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/font.hpp>
 
 #include "logger.hpp"
 
 namespace godot {
+
+class Frame_t {
+  public:
+    Rect2 frame;
+    Color color;
+
+    // Helper function to get the top left position of the frame
+    inline Vector2 get_pos() const {return frame.position;}
+    inline void set_pos(const Vector2 pos) {frame.set_position(pos);}
+    inline int x() const {return frame.position.x;}
+    inline int y() const {return frame.position.y;}
+
+    // Useful to get different coordinates of the frame
+    inline Vector2 top_left() const {return Vector2(frame.position.x, frame.position.y);}
+    inline Vector2 top_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y);}
+    inline Vector2 bottom_left() const {return Vector2(frame.position.x, frame.position.y + frame.size.y);}
+    inline Vector2 bottom_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y + frame.size.y);}
+
+    // Helper function to get or set the size of the frame
+    inline Vector2 get_size() const {return frame.get_size();}
+    inline void set_size(const Vector2 size) {frame.set_size(size);}
+    inline uint x_size() const {return frame.size.x;}
+    inline uint y_size() const {return frame.size.y;}
+};
+
+class Line_t {
+  public:
+    Color color;
+    float width;
+    Ref<Font> font;
+
+    inline Color get_color() const {return color;}
+    inline void set_color(const Color new_color) {color = new_color;}
+
+    inline float get_width() const {return width;}
+    inline void set_width(const float new_width) {width = new_width;}
+};
+
+class Data_t : public Line_t {
+  public:
+    Vector2 x_range;
+    Vector2 y_range;
+    Vector2 data;
+    Vector2 norm_data;
+};
 
 class Graph_2D : public Control {
 
@@ -39,47 +85,13 @@ class Graph_2D : public Control {
 
   private:
     void _draw_window();
-    void _draw_display_frame();
+    void _draw_display();
     void _draw_grids();
     void _draw_axis();
     void _draw_ticks();
 
     void _calculate_grid_spacing();
     void _init();
-
-    struct Frame_t {
-      Rect2 frame;
-      Color color;
-
-      // Helper function to get the top left position of the frame
-      inline Vector2 get_pos() const {return frame.position;}
-      inline void set_pos(const Vector2 pos) {frame.set_position(pos);}
-      inline int x() const {return frame.position.x;}
-      inline int y() const {return frame.position.y;}
-
-      // Useful to get different coordinates of the frame
-      inline Vector2 top_left() const {return Vector2(frame.position.x, frame.position.y);}
-      inline Vector2 top_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y);}
-      inline Vector2 bottom_left() const {return Vector2(frame.position.x, frame.position.y + frame.size.y);}
-      inline Vector2 bottom_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y + frame.size.y);}
-
-      // Helper function to get or set the size of the frame
-      inline Vector2 get_size() const {return frame.get_size();}
-      inline void set_size(const Vector2 size) {frame.set_size(size);}
-      inline uint x_size() const {return frame.size.x;}
-      inline uint y_size() const {return frame.size.y;}
-    };
-
-    struct Line_t {
-      Color color;
-      float width;
-
-      inline Color get_color() const {return color;}
-      inline void set_color(const Color new_color) {color = new_color;}
-
-      inline float get_width() const {return width;}
-      inline void set_width(const float new_width) {width = new_width;}
-    };
 
     bool _initialized;
     Vector2 _grid_spacing;
@@ -88,11 +100,14 @@ class Graph_2D : public Control {
 
     // Frame related properties
     Frame_t _window;
-    Frame_t _display_frame;
+    Frame_t _display;
 
     // Line properties
     Line_t _axis;
     Line_t _grid;
+
+    // Create data class
+    Data_t some_data;
 
     // Color properties
     Color white = Color(1.0, 1.0, 1.0, 1.0);
