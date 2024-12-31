62e987c224a17ddefad05ad6ebd2f23b319f5f5c
KhalidOwlWalid <khalidowlwalid@gmail.com>
2024-12-31 12:40:06 +0000

    [feature] gdscript for dynamic plotting

--- demo/test.gd
+++ demo/test.gd
@@ -1,14 +1,23 @@
 extends Graph_2D
 
+var last_tick = Time.get_ticks_usec()
+var graph_node
+var data
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	graph_node = get_node("/root/Control/Graph_2D")
+	data = PackedVector2Array()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func _on_mouse_entered() -> void:
-	print("I am in here")
+	var tick = Time.get_ticks_usec()
+	if (tick - last_tick > 0.01e6):
+		data.push_back(Vector2(tick, randf_range(-10, 10)))
+		graph_node.set("_data1.packed_v2_data", data)
+		last_tick = Time.get_ticks_usec()
+#
+		queue_redraw()
+	#if (tick - last_tick > 3e6):
