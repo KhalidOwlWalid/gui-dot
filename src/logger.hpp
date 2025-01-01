a7e5071781c89f16fabbbcfb43dcea8618c871d3
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-01-01 16:01:51 +0000

    [refactor] Refactor logger to add more log levels

--- src/logger.hpp
+++ src/logger.hpp
@@ -3,7 +3,30 @@
 
 #include <godot_cpp/variant/utility_functions.hpp>
 
-// TODO (Khalid): Add logging level
-#define LOG(...) UtilityFunctions::print("(class)", __class__, " : (func)", __func__, " - ", __VA_ARGS__);
+enum class LogLevel {
+  DEBUG,
+  INFO,
+  WARNING,
+  ERROR,
+};
 
-#endif
+constexpr LogLevel DEBUG = LogLevel::DEBUG;
+constexpr LogLevel INFO = LogLevel::INFO;
+constexpr LogLevel WARNING = LogLevel::WARNING;
+constexpr LogLevel ERROR = LogLevel::ERROR;
+
+#define LOG(level, ...)                                                                                                       \
+  do {                                                                                                                        \
+    switch (level) {                                                                                                          \
+      case LogLevel::DEBUG:                                                                                                   \
+        UtilityFunctions::print("[DEBUG] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);            \
+      case LogLevel::INFO:                                                                                                    \
+        UtilityFunctions::print("[INFO] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);             \
+      case LogLevel::WARNING:                                                                                                 \
+        UtilityFunctions::push_warning("(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);                 \
+      case LogLevel::ERROR:                                                                                                   \
+        UtilityFunctions::push_error("(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);                   \
+    }                                                                                                                         \
+  } while (0)                                         
+
+#endif // LOGGER_HPP
