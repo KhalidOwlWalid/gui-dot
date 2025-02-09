027f87b4017e90d725e26d2bd53ed7ca9a2647f8
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-02-09 21:47:50 +0000

    [feature] Add color to log

--- src/logger.hpp
+++ src/logger.hpp
@@ -19,16 +19,18 @@ constexpr LogLevel ERROR = LogLevel::ERROR;
   do {                                                                                                                        \
     switch (level) {                                                                                                          \
       case LogLevel::DEBUG:                                                                                                   \
-        UtilityFunctions::print("[DEBUG] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);            \
+        UtilityFunctions::print_rich("[color=yellow][DEBUG] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__, "[/color]");             \
         break;                                                                                                                \
       case LogLevel::INFO:                                                                                                    \
         UtilityFunctions::print("[INFO] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);             \
         break;                                                                                                                \
       case LogLevel::WARNING:                                                                                                 \
         UtilityFunctions::push_warning("(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);                 \
+        UtilityFunctions::print_rich("[color=orange][WARNING] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__, "[/color]");             \
         break;                                                                                                                \
       case LogLevel::ERROR:                                                                                                   \
         UtilityFunctions::push_error("(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__);                   \
+        UtilityFunctions::print_rich("[color=red][ERROR] ", "(class: ", __class__, ")", "(func: ", __func__, ") - ", __VA_ARGS__, "[/color]");             \
         break;                                                                                                                \
     }                                                                                                                         \
   } while (0)                                         
