700dc1a6b6242f2943549f8ca8c7e4c6188acd72
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-03-18 23:00:00 +0000

    [chore] Change indentation size from 2 to 4

--- src/graph_2d.hpp
+++ src/graph_2d.hpp
@@ -21,286 +21,286 @@ using namespace godot;
 using uf = UtilityFunctions;
 
 class Frame_t {
-  public:
-    Rect2 frame;
-    Color color;
+	public:
+		Rect2 frame;
+		Color color;
 
-    // Helper function to get the top left position of the frame
-    inline Vector2 get_pos() const {return frame.position;}
-    inline void set_pos(const Vector2 pos) {frame.set_position(pos);}
-    inline int x() const {return frame.position.x;}
-    inline int y() const {return frame.position.y;}
+		// Helper function to get the top left position of the frame
+		inline Vector2 get_pos() const {return frame.position;}
+		inline void set_pos(const Vector2 pos) {frame.set_position(pos);}
+		inline int x() const {return frame.position.x;}
+		inline int y() const {return frame.position.y;}
 
-    // Useful to get different coordinates of the frame
-    inline Vector2 top_left() const {return Vector2(frame.position.x, frame.position.y);}
-    inline Vector2 top_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y);}
-    inline Vector2 bottom_left() const {return Vector2(frame.position.x, frame.position.y + frame.size.y);}
-    inline Vector2 bottom_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y + frame.size.y);}
+		// Useful to get different coordinates of the frame
+		inline Vector2 top_left() const {return Vector2(frame.position.x, frame.position.y);}
+		inline Vector2 top_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y);}
+		inline Vector2 bottom_left() const {return Vector2(frame.position.x, frame.position.y + frame.size.y);}
+		inline Vector2 bottom_right() const {return Vector2(frame.position.x + frame.size.x, frame.position.y + frame.size.y);}
 
-    // Helper function to get or set the size of the frame
-    inline Vector2 get_size() const {return frame.get_size();}
-    inline void set_size(const Vector2 size) {frame.set_size(size);}
-    inline uint x_size() const {return frame.size.x;}
-    inline uint y_size() const {return frame.size.y;}
+		// Helper function to get or set the size of the frame
+		inline Vector2 get_size() const {return frame.get_size();}
+		inline void set_size(const Vector2 size) {frame.set_size(size);}
+		inline uint x_size() const {return frame.size.x;}
+		inline uint y_size() const {return frame.size.y;}
 };
 
 class Line_t {
 
-  friend class Axis_t;
-  friend class Graph_2D;
+	friend class Axis_t;
+	friend class Graph_2D;
 
-  Color color;
-  float width;
-  Ref<Font> font;
+	Color color;
+	float width;
+	Ref<Font> font;
 
-  public:
-    inline Color get_color() const {return color;}
-    inline void set_color(const Color new_color) {color = new_color;}
+	public:
+		inline Color get_color() const {return color;}
+		inline void set_color(const Color new_color) {color = new_color;}
 
-    inline float get_width() const {return width;}
-    inline void set_width(const float new_width) {width = new_width;}
+		inline float get_width() const {return width;}
+		inline void set_width(const float new_width) {width = new_width;}
 
 };
 
 class Axis_t : public Line_t {
-  String x_label;
-  String y_label;
+	String x_label;
+	String y_label;
 };
 
 class Data_t : public Line_t {
 
-  const String __class__ = "Data_t";
-  friend class Graph_2D;
+	const String __class__ = "Data_t";
+	friend class Graph_2D;
 
-  Vector2 x_range;
-  Vector2 y_range;
-  // TODO: Once data server is implemented, this should be move there?
-  PackedVector2Array packed_v2_data; // Stores all of the data
-  PackedVector2Array pixel_pos_v2_data;
-  PackedVector2Array lod_data; // Level Of Detail data - only plot visible data
-  float ts; // Calculated Sample time
-  bool use_antialiased;
-  String keyword;
-  String unit;
-  Dictionary data_dict;
+	Vector2 x_range;
+	Vector2 y_range;
+	// TODO: Once data server is implemented, this should be move there?
+	PackedVector2Array packed_v2_data; // Stores all of the data
+	PackedVector2Array pixel_pos_v2_data;
+	PackedVector2Array lod_data; // Level Of Detail data - only plot visible data
+	float ts; // Calculated Sample time
+	bool use_antialiased;
+	String keyword;
+	String unit;
+	Dictionary data_dict;
 
-  bool is_y_axis_lock = false;
-  bool is_x_axis_lock = false;
+	bool is_y_axis_lock = false;
+	bool is_x_axis_lock = false;
 
-  public:
+	public:
 
-    // TODO: Create an assertion to ensure x_max is always bigger than x_min
-    template <typename T> void set_x_max(const T val) {
-      x_range[1] = val;
-    };
+		// TODO: Create an assertion to ensure x_max is always bigger than x_min
+		template <typename T> void set_x_max(const T val) {
+			x_range[1] = val;
+		};
 
-    template <typename T> void set_x_min(const T val) {
-      x_range[0] = val;
-    }
+		template <typename T> void set_x_min(const T val) {
+			x_range[0] = val;
+		}
 
-    template <typename T> void set_y_max(const T val) {
-      y_range[1] = val;
-    }
+		template <typename T> void set_y_max(const T val) {
+			y_range[1] = val;
+		}
 
-    template <typename T> void set_y_min(const T val) {
-      y_range[0] = val;
-    }
+		template <typename T> void set_y_min(const T val) {
+			y_range[0] = val;
+		}
 
-    inline double x_max() {return x_range[1]; }
-    inline double x_min() {return x_range[0]; }
-    inline double y_max() {return y_range[1]; }
-    inline double y_min() {return y_range[0]; }
+		inline double x_max() {return x_range[1]; }
+		inline double x_min() {return x_range[0]; }
+		inline double y_max() {return y_range[1]; }
+		inline double y_min() {return y_range[0]; }
 
-    template <typename T> T get_x_diff() {
-      T diff = x_range[1] - x_range[0];
-      return diff;
-    }
+		template <typename T> T get_x_diff() {
+			T diff = x_range[1] - x_range[0];
+			return diff;
+		}
 
-    template <typename T> T get_y_diff() const {
-      T diff = y_range[1] - y_range[0];
-      return diff;
-    }
+		template <typename T> T get_y_diff() const {
+			T diff = y_range[1] - y_range[0];
+			return diff;
+		}
 
-    // Sets the range for the x and y axis by obtaining the min and max value of the data
-    void set_range() {
-      Vector2 min = lod_data[0];
-      Vector2 max = lod_data[0];
-      // TODO: Find a better optimized way to do this
-      for (size_t i = 0; i < lod_data.size(); i++) {
-          min.x = std::min(min.x, lod_data[i].x);
-          max.x = std::max(max.x, lod_data[i].x);
-          min.y = std::min(min.y, lod_data[i].y);
-          max.y = std::max(max.y, lod_data[i].y);
-      }
-      // Only update the range if the axis is not lock
-      x_range = is_x_axis_lock ? x_range: Vector2(min.x, max.x);
-      // A scale of 0.1 is added for both min and max y to ensure the data is not drawn at the border of the display
-      y_range = is_y_axis_lock ? y_range: Vector2(min.y, max.y) + Vector2(0.1, 0.1) * Vector2(min.y, max.y);
-    }
+		// Sets the range for the x and y axis by obtaining the min and max value of the data
+		void set_range() {
+			Vector2 min = lod_data[0];
+			Vector2 max = lod_data[0];
+			// TODO: Find a better optimized way to do this
+			for (size_t i = 0; i < lod_data.size(); i++) {
+					min.x = std::min(min.x, lod_data[i].x);
+					max.x = std::max(max.x, lod_data[i].x);
+					min.y = std::min(min.y, lod_data[i].y);
+					max.y = std::max(max.y, lod_data[i].y);
+			}
+			// Only update the range if the axis is not lock
+			x_range = is_x_axis_lock ? x_range: Vector2(min.x, max.x);
+			// A scale of 0.1 is added for both min and max y to ensure the data is not drawn at the border of the display
+			y_range = is_y_axis_lock ? y_range: Vector2(min.y, max.y) + Vector2(0.1, 0.1) * Vector2(min.y, max.y);
+		}
 
-    void set_y_range(float min, float max) {
-      if (min > max) {
-        LOG(WARNING, "min > max! Defaults to min,", y_min(), " and max,", y_max());
-        return;
-      }
-      y_range[0] = min;
-      y_range[1] = max;
-    }
+		void set_y_range(float min, float max) {
+			if (min > max) {
+				LOG(WARNING, "min > max! Defaults to min,", y_min(), " and max,", y_max());
+				return;
+			}
+			y_range[0] = min;
+			y_range[1] = max;
+		}
 
-    void set_x_range(float min, float max) {
-      if (min > max) {
-        LOG(WARNING, "min > max! Defaults to min,", x_min(), " and max,", x_max());
-        return;
-      }
-      x_range[0] = min;
-      x_range[1] = max;
-    }
+		void set_x_range(float min, float max) {
+			if (min > max) {
+				LOG(WARNING, "min > max! Defaults to min,", x_min(), " and max,", x_max());
+				return;
+			}
+			x_range[0] = min;
+			x_range[1] = max;
+		}
 
-    void calculate_sample_time(float window_sample_size) {
-      float ts_sum = 0;
-      // In order to get the sample time with the size of the moving window, we need to add one more since we are calculating
-      // the next time minus the current time
-      for (size_t i = packed_v2_data.size() - window_sample_size - 1; i < packed_v2_data.size() - 1; i++) {
-        ts_sum += (packed_v2_data[i + 1].x - packed_v2_data[i].x);
-      }
-      ts = ts_sum / window_sample_size;
-    }
+		void calculate_sample_time(float window_sample_size) {
+			float ts_sum = 0;
+			// In order to get the sample time with the size of the moving window, we need to add one more since we are calculating
+			// the next time minus the current time
+			for (size_t i = packed_v2_data.size() - window_sample_size - 1; i < packed_v2_data.size() - 1; i++) {
+				ts_sum += (packed_v2_data[i + 1].x - packed_v2_data[i].x);
+			}
+			ts = ts_sum / window_sample_size;
+		}
 
-    void info() const {
-      LOG(INFO, "Keyword: ", keyword, " - Current V2 data: ", lod_data);
-    }
+		void info() const {
+			LOG(INFO, "Keyword: ", keyword, " - Current V2 data: ", lod_data);
+		}
 
 };
 
 struct sliding_window_info {
-  float t_min;
-  float t_max;
+	float t_min;
+	float t_max;
 
-  Vector2 range() {return Vector2(t_min, t_max);}
+	Vector2 range() {return Vector2(t_min, t_max);}
 };
 
 class Graph_2D : public Control {
 
-  GDCLASS(Graph_2D, Control);
-  const String __class__ = "Graph_2D";
+	GDCLASS(Graph_2D, Control);
+	const String __class__ = "Graph_2D";
 
-  public:
+	public:
 
-    enum Status {
-      FAIL,
-      SUCCESS
-    };
+		enum Status {
+			FAIL,
+			SUCCESS
+		};
 
-    Graph_2D();
-    ~Graph_2D();
+		Graph_2D();
+		~Graph_2D();
 
-    void _process(double delta) override;
+		void _process(double delta) override;
 
-    void set_window_background_color(const Color &color);
-    Color get_window_background_color() const;
+		void set_window_background_color(const Color &color);
+		Color get_window_background_color() const;
 
-    void set_window_size(const Vector2 &win_size);
-    Vector2 get_window_size() const;
+		void set_window_size(const Vector2 &win_size);
+		Vector2 get_window_size() const;
 
-    void set_display_background_color(const Color &color);
-    Color get_display_background_color() const;
+		void set_display_background_color(const Color &color);
+		Color get_display_background_color() const;
 
-    void set_grid_size(const Vector2 &grid_size);
-    Vector2 get_grid_size() const;
+		void set_grid_size(const Vector2 &grid_size);
+		Vector2 get_grid_size() const;
 
-    void set_data(const PackedVector2Array &data, const int n);
-    PackedVector2Array get_data(const int n) const;
+		void set_data(const PackedVector2Array &data, const int n);
+		PackedVector2Array get_data(const int n) const;
 
-    void set_y_range(const String keyword, const float min, const float max);
-    Vector2 get_y_range(const String keyword) const;
+		void set_y_range(const String keyword, const float min, const float max);
+		Vector2 get_y_range(const String keyword) const;
 
-    void set_x_range(const String keyword, const float min, const float max);
-    Vector2 get_x_range(const String keyword) const;
+		void set_x_range(const String keyword, const float min, const float max);
+		Vector2 get_x_range(const String keyword) const;
 
-    void set_antialiased_flag(const bool flag);
+		void set_antialiased_flag(const bool flag);
 
-    Status add_new_data_with_keyword(const String &keyword, const PackedVector2Array &data, const Color color);
-    Status update_data_with_keyword(const String &keyword, const PackedVector2Array &data);
-    PackedVector2Array get_data_with_keyword(const String &keyword) const;
-    Status append_data_with_keyword(const String &keyword, const float &data);
-    Status append_data_array_with_keyword(const String &keyword, const PackedVector2Array &data_array);
+		Status add_new_data_with_keyword(const String &keyword, const PackedVector2Array &data, const Color color);
+		Status update_data_with_keyword(const String &keyword, const PackedVector2Array &data);
+		PackedVector2Array get_data_with_keyword(const String &keyword) const;
+		Status append_data_with_keyword(const String &keyword, const float &data);
+		Status append_data_array_with_keyword(const String &keyword, const PackedVector2Array &data_array);
 
 
-    void set_data_line_color(const Color &color, const int n);
-    Color get_data_line_color(const int n) const;
+		void set_data_line_color(const Color &color, const int n);
+		Color get_data_line_color(const int n) const;
 
-    // TODO: Populate this
-    void get_font();
-    Ref<Font> set_font();
+		// TODO: Populate this
+		void get_font();
+		Ref<Font> set_font();
 
-    void _draw() override;
+		void _draw() override;
 
-  protected:
-    static void _bind_methods();
-    void _notification(const int p_what);
+	protected:
+		static void _bind_methods();
+		void _notification(const int p_what);
 
-  private:
-    void _draw_window();
-    void _draw_display();
-    void _draw_grids();
-    void _draw_axis();
-    void _draw_ticks();
-    void _draw_plot();
-    void _preprocess_data();
-    void _init_font();
+	private:
+		void _draw_window();
+		void _draw_display();
+		void _draw_grids();
+		void _draw_axis();
+		void _draw_ticks();
+		void _draw_plot();
+		void _preprocess_data();
+		void _init_font();
 
-    void _calculate_grid_spacing();
-    void _init();
+		void _calculate_grid_spacing();
+		void _init();
 
-    bool use_antialiased = false;
-    Vector2 _coordinate_to_pixel(const Vector2 &data, const Vector2 &x_range, const Vector2 &y_range);
+		bool use_antialiased = false;
+		Vector2 _coordinate_to_pixel(const Vector2 &data, const Vector2 &x_range, const Vector2 &y_range);
 
-    // TODO: Make this a template
-    String _format_axis_label(const float &val, int dp);
+		// TODO: Make this a template
+		String _format_axis_label(const float &val, int dp);
 
-    bool _initialized {false};
-    Vector2 _grid_spacing;
-    Vector2 _n_grid {Vector2(10, 5)};
-    Vector2 _frame_margin {Vector2(100, 100)};
-    Ref<Font> _font_manager;
+		bool _initialized {false};
+		Vector2 _grid_spacing;
+		Vector2 _n_grid {Vector2(10, 5)};
+		Vector2 _frame_margin {Vector2(100, 100)};
+		Ref<Font> _font_manager;
 
-    // Frame related properties
-    Frame_t _window;
-    Frame_t _display;
+		// Frame related properties
+		Frame_t _window;
+		Frame_t _display;
 
-    // Line properties
-    Line_t _axis;
-    Line_t _grid;
+		// Line properties
+		Line_t _axis;
+		Line_t _grid;
 
-    // Data vector allows us to store multiple sets of data
-    // for multi-axis plot
-    std::vector<Data_t> data_vector;
-    PackedVector2Array test_data;
+		// Data vector allows us to store multiple sets of data
+		// for multi-axis plot
+		std::vector<Data_t> data_vector;
+		PackedVector2Array test_data;
 
-    uint64_t ticks;
-    uint64_t last_update_ticks;
+		uint64_t ticks;
+		uint64_t last_update_ticks;
 
-    Node2D *_label_parent;
+		Node2D *_label_parent;
 
-    // Color properties
-    Color white = Color(1.0, 1.0, 1.0, 1.0);
-    Color grey = Color(0.17, 0.17, 0.17, 1.0);
-    Color black = Color(0.07, 0.07, 0.07, 1.0);
-    Color red = Color(1.0, 0.07, 0.07, 1.0);
-    Color gd_grey = Color::hex(0x363d4a);
-    Color gd_blue = Color::hex(0x252b34);
-    Color green = Color::hex(0x469d5a);
+		// Color properties
+		Color white = Color(1.0, 1.0, 1.0, 1.0);
+		Color grey = Color(0.17, 0.17, 0.17, 1.0);
+		Color black = Color(0.07, 0.07, 0.07, 1.0);
+		Color red = Color(1.0, 0.07, 0.07, 1.0);
+		Color gd_grey = Color::hex(0x363d4a);
+		Color gd_blue = Color::hex(0x252b34);
+		Color green = Color::hex(0x469d5a);
 
-    // Temporary constant use
-    const int font_size = 16;
-    const int font_margin = font_size + 20;
-    const int label_margin = 20;
-    const int dp = 2;
-    int max_digit_size = 1;
+		// Temporary constant use
+		const int font_size = 16;
+		const int font_margin = font_size + 20;
+		const int label_margin = 20;
+		const int dp = 2;
+		int max_digit_size = 1;
 
-    float sliding_window_duration = 15.0;
+		float sliding_window_duration = 15.0;
 
-    sliding_window_info _sw_info;
+		sliding_window_info _sw_info;
 
 };
 
