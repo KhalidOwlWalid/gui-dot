8587ce273303123b7e734a2dfc7435de122a753e
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-09-07 23:09:12 +0100

    [chore] Add some static data test on mavlink_node

--- gdscript/mavlink_node.gd
+++ gdscript/mavlink_node.gd
@@ -1,4 +1,4 @@
-@tool
+# @tool
 extends Node
 
 const Guidot_Data := preload("res://gdscript/components/guidot_data.gd")
@@ -8,24 +8,32 @@ const Guidot_Data := preload("res://gdscript/components/guidot_data.gd")
 @onready var curr_t: float = 0
 @onready var last_update_ms: int = Time.get_ticks_msec()
 @onready var init_ms: int = Time.get_ticks_msec()
+@onready var data_transmitted: bool = false
 
 signal data_received
-signal data_transmitted
 
 func append_point(new_point: Vector2) -> void:
 	data.append(new_point)
 	data_received.emit()
 
 func _ready() -> void:
+	for i in range(100):
+		data.append(Vector2(i, i))
 	pass
 
 func _mouse_cursor_data(delta: float) -> void:
 	var curr_ms: int = Time.get_ticks_msec()
-	if (curr_ms - last_update_ms > 10):
+	if (curr_ms - last_update_ms > 1000):
 		var relative_ms: int = curr_ms - init_ms
 		var curr_mouse_pos = self.get_viewport().get_mouse_position()
 		append_point(Vector2(float(relative_ms)/1000, curr_mouse_pos.x/1920))
 		last_update_ms = Time.get_ticks_msec()
 
 func _process(delta: float) -> void:
-	_mouse_cursor_data(delta)
+	# _mouse_cursor_data(delta)
+	data = PackedVector2Array()
+	if !(data_transmitted):
+		var a = 0.2
+		for i in range(3000):
+			data.append(Vector2(i * a , i))
+		data_received.emit()
