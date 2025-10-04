bc8e8fb83097015259157c4d1a5449f12101dfe8
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-10-04 10:17:39 +0100

    [feat] Add circles to allow for visual points for resizing graphs

--- gdscript/guidot_tmp.gd
+++ gdscript/guidot_tmp.gd
@@ -1,14 +1,14 @@
 @tool
-extends ColorRect
+extends PanelContainer
 
-var axis_height: int
-
+# TODO: Recheck this, since if this gets inherited, then it will use its local position
+func top_left() -> Vector2:
+	var _top_left: Vector2
+	_top_left = Vector2(0, 0)
+	return _top_left
+	
 func _ready() -> void:
-	self.color = Color.BLACK
-	# self.offset_left = 50
-	self.set_anchors_preset(Control.LayoutPreset.PRESET_CENTER)
-	self.axis_height = 10
-	self.offset_left = 10
-	self.offset_right = 20
-	self.offset_top = 10
-	self.offset_bottom = 10 + self.axis_height
+	queue_redraw()
+	
+func _draw() -> void:
+	self.draw_circle(self.top_left(), 2, Color.RED)
