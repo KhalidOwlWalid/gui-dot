82d03a09b0930ec385b20675d92c2a61df8a746b
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-10-13 23:08:57 +0100

    [checkpoint] Initial checkpoint for a simple server and client

--- gdscript/components/guidot_data.gd
+++ gdscript/components/guidot_data.gd
@@ -1,2 +1,30 @@
-class_name Guidot_Data_Core
-extends Node
+#class_name Guidot_Data_Core
+extends Control
+
+@onready var frequency: float = 0
+@onready var unit: String = ""
+@onready var description: String = ""
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func set_unit(unit: String) -> void:
+	pass
+
+func get_unit() -> String:
+	return String()
+
+func set_description(description: String) -> void:
+	pass
+
+func get_description() -> String:
+	return String()
+
+func set_frequency(freq: float) -> void:
+	pass
+
+func get_freqeuncy() -> float:
+	return 0
