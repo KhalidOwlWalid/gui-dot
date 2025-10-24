a169b285cb84f1bb5ab3552568dadbe3ac53741d
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-10-24 20:39:52 +0100

    [chore] Create examples of GUI for data selection

--- gdscript/mavlink_node.gd
+++ gdscript/mavlink_node.gd
@@ -37,12 +37,12 @@ func _mouse_cursor_data(delta: float) -> void:
 		var curr_mouse_pos = self.get_viewport().get_mouse_position()
 
 		# Mimic no data points
-		if (float(Time.get_ticks_msec())/1000 > 5.0) and float(Time.get_ticks_msec())/1000 < 10.0 \
-			or (float(Time.get_ticks_msec())/1000 > 12.0) and float(Time.get_ticks_msec())/1000 < 20.0:
-			append_point(Vector2(curr_s, curr_mouse_pos.x/1920))
-			self._dc_mouse_cursor_x.add_data_point(curr_mouse_pos.x)
-			self._dc_mouse_cursor_y.add_data_point(curr_mouse_pos.y)
-			last_update_ms = Time.get_ticks_msec()
+		# if (float(Time.get_ticks_msec())/1000 > 5.0) and float(Time.get_ticks_msec())/1000 < 10.0 \
+		# 	or (float(Time.get_ticks_msec())/1000 > 12.0) and float(Time.get_ticks_msec())/1000 < 20.0:
+		append_point(Vector2(curr_s, curr_mouse_pos.x/1920))
+		self._dc_mouse_cursor_x.add_data_point(curr_mouse_pos.x)
+		self._dc_mouse_cursor_y.add_data_point(curr_mouse_pos.y)
+		last_update_ms = Time.get_ticks_msec()
 
 func _physics_process(delta: float) -> void:
 	_mouse_cursor_data(delta)	
