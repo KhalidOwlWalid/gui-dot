fefef2283450dfe9cc3746e590dbbe6af113a621
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-10-27 21:53:51 +0000

    [feat] Modularize server config and data subscriber manager

--- gdscript/components/menu/server_config.gd
+++ gdscript/components/menu/server_config.gd
@@ -1,30 +1,53 @@
 class_name Guidot_Server_Config
 extends Guidot_Panel2
 
+var data_subscriber_manager: Guidot_Data_Sub_Manager
+
+func _on_subscribe_pressed() -> void:
+	self.data_subscriber_manager.visible = true
+
+func _on_close_submenu_button_pressed(panel: Node) -> void:
+	panel.visible = false
+
+func register_data_sub_manager(dsub_node: Guidot_Data_Sub_Manager) -> void:
+	self.data_subscriber_manager = dsub_node
+	# TODO (Khalid): I do not like the way that this basically gets opened inside the server
+	# selection panel itself, but I am struggling in making the data subscriber manager to open separately
+	# on its own
+	self.add_child(self.data_subscriber_manager)
+	
 func _ready() -> void:
 	super._ready()
 
 	var vbox: VBoxContainer = VBoxContainer.new()
 	self.add_child_to_container(vbox)
 
-	var button = Button.new()
-	button.text = "Server Selection 1"
-	vbox.add_child(button)
+	var header_label = Label.new()
+	header_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	header_label.text = "Server Selection 1"
+	vbox.add_child(header_label)
 
 	# All server configuration settings
-	var graph_buffer_mode_label = Guidot_Utils.create_label_row("Current mode", "Realtime", Vector2(200, 20))
 	var server_selection = Guidot_Utils.create_dropdown_selection_row("Server Node", ["Khalid", "Alia"], Vector2(200, 20))
 
-	vbox.add_child(graph_buffer_mode_label)
-	vbox.add_child(server_selection)
+	var margin_cont1: MarginContainer = MarginContainer.new()
+	var subscribe_data_button: Button = Button.new()
+	margin_cont1.add_child(subscribe_data_button)
+	margin_cont1.add_theme_constant_override("margin_left", 10)
+	margin_cont1.add_theme_constant_override("margin_right", 10)
+	margin_cont1.add_theme_constant_override("margin_bottom", 10)
+	subscribe_data_button.text = "+ Subscribe to data"
+	subscribe_data_button.pressed.connect(self._on_subscribe_pressed)
 
-	# var subscribe_data_button: Button = Button.new()
-	# subscribe_data_margin_container.add_child(subscribe_data_button)
-	# subscribe_data_button.text = "+ Subscribe to data"
-	# subscribe_data_button.pressed.connect(_on_subscribe_pressed)
+	vbox.add_child(server_selection)
+	vbox.add_child(margin_cont1)
 
 	# # TODO (Khalid): Use a scroll container to allow us to go through all of the subscribed data
 	# var sub_data_scroll_cont: ScrollContainer = ScrollContainer.new()
 	# sub_data_scroll_cont.custom_minimum_size = Vector2(100, 200)
 	# var sub_data_vbox: VBoxContainer = VBoxContainer.new()
 	# sub_data_scroll_cont.add_child(sub_data_vbox)
+
+func _process(delta: float) -> void:
+	pass
+	# pass
