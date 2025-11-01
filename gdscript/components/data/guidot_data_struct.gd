39ce57249a2331ad9847e2159f832150899c1837
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-11-01 23:42:41 +0000

    [refactor] Refactor guidot data setup properties

--- gdscript/components/data/guidot_data_struct.gd
+++ gdscript/components/data/guidot_data_struct.gd
@@ -7,6 +7,7 @@ var _line_color: Color = Color.RED
 var _line_color_str: String = "red"
 var _min: float = 0
 var _max: float = 1
+var _freq: float = 60.0
 
 var _metadata: Dictionary = {
 	"unique_name": "",
@@ -17,43 +18,51 @@ var _metadata: Dictionary = {
 	"line_color": "red",
 	"min": self._min,
 	"max": self._max, 
+	"expected_frequency": self._freq,
 }
 
-# var _color_options: Dictionary = {
-# 	"red": Color.RED,
-# 	"white": Color.WHITE,
-# }
+# Setters
+func set_line_color(line_color_str: String) -> void:
+	self._line_color = Guidot_Utils.get_color(line_color_str)
+
+func set_min(min_val: float) -> void:
+	self._min = min_val
+	self._metadata["min"] = self._min
+
+func set_max(max_val: float) -> void:
+	self._max = max_val
+	self._metadata["max"] = self._max
+
+func set_line_color_str(color_str: String) -> void:
+	self._line_color_str = color_str
+	self._metadata["line_color"] = self._line_color_str
+
+func set_unique_name(unique_name_str: String) -> void:
+	self._name = unique_name_str
+	self._metadata["unique_name"] = self._name
+	self._metadata["data_name"] = self._name
+
+func set_unit(unit: String) -> void:
+	self._unit = unit
+	self._metadata["unit"] = self._unit
+
+func set_description(descr: String) -> void:
+	self._description = descr
+	self._metadata["description"] = self._description
+
+func set_frequency(freq: float) -> void:
+	self._freq = freq
+	self._metadata["expected_frequency"] = self._freq
 
 func line_color_options() -> Dictionary:
 	return self._color_options
 
-func set_line_color(line_color_str: String) -> void:
-	self._line_color = Guidot_Utils.get_color(line_color_str)
-
+# Getters
 func get_line_color() -> Color:
 	return self._line_color
 
 func get_min_max() -> Vector2:
 	return Vector2(self._min, self._max)
-
-func _set_metadata(name: String, unit: String, description: String, min: float, max: float, line_color: String) -> void:
-	self._metadata["unique_name"] = name
-	self._metadata["data_name"] = name
-	self._metadata["unit"] = unit
-	self._metadata["description"] = description
-	self._metadata["line_color"] = line_color
-	self._metadata["min"] = min
-	self._metadata["max"] = max
-
-func setup_properties(name: String, unit: String, description: String, min: float, max: float, line_color: String = "red"):
-	self._name = name
-	self._unit = unit
-	self._description = description
-	self._min = min
-	self._max = max
-	self._line_color_str = line_color
-	self.set_line_color(line_color)
-	self._set_metadata(self._name, self._unit, self._description, self._min, self._max, self._line_color_str)
 
 func get_name() -> String:
 	return self._name
@@ -66,3 +75,22 @@ func get_unit() -> String:
 
 func get_unique_id() -> int:
 	return self._metadata["unique_id"]
+
+
+func _set_metadata(name: String, unit: String, description: String, min: float, max: float, line_color: String) -> void:
+	# self.set_unique_name(name)
+	self._metadata["data_name"] = name
+	self._metadata["unit"] = unit
+	self._metadata["description"] = description
+	self._metadata["line_color"] = line_color
+	self._metadata["min"] = min
+	self._metadata["max"] = max
+
+func setup_properties(name: String, unit: String, description: String, min: float, max: float, line_color: String = "red"):
+	self.set_unique_name(name)
+	self.set_unit(unit)
+	self.set_description(description)
+	self.set_min(min)
+	self.set_max(max)
+	self.set_line_color_str(line_color)
+	self.set_line_color(line_color)
