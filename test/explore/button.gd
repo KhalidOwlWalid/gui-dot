afd9d7e74c8c678bc66f4143b4f49055bb1e4cf2
KhalidOwlWalid <khalidowlwalid@gmail.com>
2026-05-04 19:11:20 +0100

    [chore] Creating example of godot menu

--- test/explore/button.gd
+++ test/explore/button.gd
@@ -1,22 +1,17 @@
 extends Button
 
-@export var icon_texture: Texture2D
-@export var icon_size: Vector2 = Vector2(30, 30)
-@onready var _setting_icon: Texture2D = load("res://addons/guidot/icons/gear_icon.png")
+var expanded := false
 
 func _ready() -> void:
-	var resized_image = self._setting_icon.get_image()
-	resized_image.resize(int(icon_size.x), int(icon_size.y))
-	set_button_icon(ImageTexture.create_from_image(resized_image))
-	
-	size = icon_size
-	# flat = true
-	
-	var empty = StyleBoxEmpty.new()
-	for state in ["normal", "disabled"]:
-		add_theme_stylebox_override(state, empty)
-	
-	set_anchors_preset(Control.LayoutPreset.PRESET_TOP_LEFT)
+	pressed.connect(_toggle)
+	add_theme_stylebox_override("focus", StyleBoxEmpty.new())
+	get_parent().get_node("hotkeys").visible = false
 
-func _process(delta: float) -> void:
-	pass
+func _toggle() -> void:
+	expanded = !expanded
+	get_parent().get_node("hotkeys").visible = expanded
+	
+	if (expanded):
+		self.text = "] Preferences"
+	else:
+		self.text = "> Preferences"
